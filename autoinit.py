15de1bf1fa7a6c9e742e63d4909d575b8ba980e9
Kosma <oversider_kosma@mail.ru>
2020-03-08 11:15:40 +0300

    Add  and  options. Update docstring.

--- autoinit.py
+++ autoinit.py
@@ -12,8 +12,23 @@ class AutoinitWarning(UserWarning, ValueError):
     pass
 
 
-def autoinit(init_or_class):
-    '''Decorator for automatic initialization instance attributes'''
+def autoinit(init_or_class, reverse=False, no_warn=False):
+    '''
+    Decorator for automatic initialization instance attributes
+
+        @autoinit
+        def __init__(self, a, b=10):
+            pass
+
+    is equivalent to
+
+        def __init__(self, a, b=10):
+            self.a = a
+            self.b = b
+    Options:
+        reverse: bool = False # call real __init__ before setting attributes
+        no_warn: bool = False # do not warn when decorator applied to not __init__
+    '''
     if _isclass(init_or_class):
         func = getattr(init_or_class, '__init__')
     elif _isfunction(init_or_class):
@@ -21,17 +36,20 @@ def autoinit(init_or_class):
     else:
         raise ValueError("autoinit decorator should be applied to class or its __init__ method")
 
-    if func.__name__  != '__init__' or func.__code__.co_name != '__init__':
-        _warn(AutoinitWarning("autoinit decorator intended to be applied only to __init__ method"))
+    if (func.__name__  != '__init__' or func.__code__.co_name != '__init__') and not no_warn:
+        _warn(AutoinitWarning("autoinit decorator intended to be applied only to __init__ method (use autoinit(no_warn=True) to suppress this warning)"))
 
     args_names = func.__code__.co_varnames[1:func.__code__.co_argcount]
 
     @_wraps(func)
     def inner(self, *args, **kwargs):
+        if reverse:
+            func(self, *args, **kwargs)
         args_vals = args[:] + func.__defaults__[len(args) - len(args_names):]
         for k, v in zip(args_names, args_vals):
             setattr(self, k, v)
-        func(self, *args, **kwargs)
+        if not reverse:
+            func(self, *args, **kwargs)
 
     if _isclass(init_or_class):
         init_or_class.__init__ = inner
